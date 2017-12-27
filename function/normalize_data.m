a8c4b5ca7cbadd830bf09cf1d77fee51ab696f93
ram narayan patro <patro.ramnarayan@gmail.com>
2017-12-27 22:10:47 +0530

    Update normalize_data.m

--- function/normalize_data.m
+++ function/normalize_data.m
@@ -1,9 +1,9 @@
 function total_data_n=normalize_data(total_data)
 for i=1:size(total_data,2)
     if(sign(min(total_data(:,i)))<0)
-        total_data_n=total_data+abs(min(total_data));
-        total_data_n=total_data_n./max(abs(total_data_n));
+        total_data_n(:,i)=total_data(:,i)+abs(min(total_data(:,i)));
+        total_data_n(:,i)=total_data_n(:,i)./max(abs(total_data_n(:,i)));
     else
-        total_data_n=(total_data./max(abs(total_data)));
+        total_data_n(:,i)=(total_data(:,i)./max(abs(total_data(:,i))));
     end
 end
